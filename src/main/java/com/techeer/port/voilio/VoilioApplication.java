5476add8ead28cd08f6a425aba2ed6cae949e09c
gilyeon00 <12venus08@naver.com>
2023-03-15 09:29:00 +0900

    Feat: Set Voilio Backend Project

---
+++ src/main/java/com/techeer/port/voilio/VoilioApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.port.voilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoilioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoilioApplication.class, args);
+	}
+
+}
