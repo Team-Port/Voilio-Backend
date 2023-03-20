175fbe1457f175e9b924ac09c773da51ed3e7b2f
kimich <ickim1218@gmail.com>
2023-03-20 16:26:46 +0900

    [#1]Feat: error 발생시 실행할 메소드를 정의한 Handler 클래스 구현

---
+++ src/main/java/com/techeer/port/voilio/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.techeer.port.voilio.global.error;
+
+import com.techeer.port.voilio.global.error.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.techeer.port.voilio.global.error.ErrorCode.INPUT_INVALID_VALUE;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler
+  protected ResponseEntity<ErrorResponse> handleRuntimeException(BusinessException e) {
+    final ErrorCode errorCode = e.getErrorCode();
+    final ErrorResponse response =
+        ErrorResponse.builder()
+            .errorMessage(errorCode.getMessage())
+            .businessCode(errorCode.getCode())
+            .build();
+    log.warn(e.getMessage());
+    return ResponseEntity.status(errorCode.getStatus()).body(response);
+  }
+
+  @ExceptionHandler
+  protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException e) {
+    final ErrorResponse response = ErrorResponse.of(INPUT_INVALID_VALUE, e.getBindingResult());
+    log.warn(e.getMessage());
+    return ResponseEntity.status(INPUT_INVALID_VALUE.getStatus()).body(response);
+  }
+}
