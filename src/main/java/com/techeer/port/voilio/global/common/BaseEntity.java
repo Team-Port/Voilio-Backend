b434bdbe5866c9c26422f80f8f0c7357dafef0e5
hangyeol <snake011128@naver.com>
2023-03-21 21:08:32 +0900

    [#3]Feat: Base 엔티티 생성

--- src/main/java/com/techeer/port/voilio/global/common/BaseEntity.java
+++ src/main/java/com/techeer/port/voilio/global/common/BaseEntity.java
@@ -1,3 +1,33 @@
 package com.techeer.port.voilio.global.common;
 
-public class BaseEntity {}
+import com.sun.istack.NotNull;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "createdAt", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updatedAt", nullable = false, updatable = false)
+    private LocalDateTime updatedAt;
+
+    @Column
+    @NotNull
+    @ColumnDefault("true")
+    private boolean isDeleted;
+
+    public boolean getIsDeleted() {
+        return this.getIsDeleted();
+    }
+}
