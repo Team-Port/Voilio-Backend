ccab8729fdbfb8c56b5aa2c55b634487b67eb629
hangyeol <snake011128@naver.com>
2023-03-21 21:07:44 +0900

    [#3]Fix: Board 엔티티 수정

--- src/main/java/com/techeer/port/voilio/domain/board/entity/Board.java
+++ src/main/java/com/techeer/port/voilio/domain/board/entity/Board.java
@@ -1,14 +1,11 @@
 package com.techeer.port.voilio.domain.board.entity;
 
+import com.techeer.port.voilio.global.common.Category;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Getter
@@ -19,34 +16,40 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer board_id;
 
+    @Column
     private String title;
 
+    @Column
     private String content;
 
-    @ElementCollection(targetClass = Integer.class)
-    private List<Integer> category;
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Category category1;
 
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Category category2;
+
+    @Column
     private String video_url;
 
+    @Column
     private String thumbnail_url;
 
     @Column
     private Boolean isPublic;
 
-    @Column(nullable = false)
-    private Boolean isDeleted;
-
     @Builder
-    public Board(Integer board_id, String title, String content, List<Integer> category, String video_url,
-        String thumbnail_url, boolean isPublic, boolean isDeleted) {
+    public Board(Integer board_id, String title, String content, Category category1, Category category2,
+        String video_url, String thumbnail_url, boolean isPublic) {
         this.board_id = board_id;
         this.title = title;
-        this.category = category;
+        this.category1 = category1;
+        this.category2 = category2;
         this.content = content;
         this.video_url = video_url;
         this.thumbnail_url = thumbnail_url;
         this.isPublic = isPublic;
-        this.isDeleted = isDeleted;
     }
 }
 
