34b6a2c8c1f9e9f92c5c57936af09ecb8357e691
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-03-21 15:31:42 +0900

    Board Category Enum 생성

---
+++ src/main/java/com/techeer/port/voilio/global/common/Category.java
@@ -0,0 +1,12 @@
+package com.techeer.port.voilio.global.common;
+
+public enum Category {
+    IT(1);
+
+    private final int value;
+    Category(int value) {this.value = value;}
+
+    public int getValue() {
+        return value;
+    }
+}
