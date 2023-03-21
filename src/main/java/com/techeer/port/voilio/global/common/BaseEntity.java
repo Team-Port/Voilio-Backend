71a68a50422a22569c402c82cc33b57033ec9a69
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-03-21 15:30:31 +0900

    feat : BaseEntity 생성

--- src/main/java/com/techeer/port/voilio/global/common/BaseEntity.java
+++ src/main/java/com/techeer/port/voilio/global/common/BaseEntity.java
@@ -1,4 +1,28 @@
 package com.techeer.port.voilio.global.common;
 
+import com.sun.istack.NotNull;
+import lombok.Builder;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDate;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+    @CreatedDate
+    private LocalDate createAt;
+
+    @LastModifiedDate
+    private LocalDate updateAt;
+
+    @Column
+    @NotNull
+    @ColumnDefault("true")
+    private boolean isDeleted;
 }
