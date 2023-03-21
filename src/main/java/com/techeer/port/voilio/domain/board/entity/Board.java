b93c140faf23d172fefc3610d98068d68cf5ea1c
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-03-21 15:30:12 +0900

    feat : Board Entity 생성

--- src/main/java/com/techeer/port/voilio/domain/board/entity/Board.java
+++ src/main/java/com/techeer/port/voilio/domain/board/entity/Board.java
@@ -1,4 +1,49 @@
 package com.techeer.port.voilio.domain.board.entity;
 
-public class Board {
+import com.sun.istack.NotNull;
+import com.techeer.port.voilio.global.common.BaseEntity;
+import com.techeer.port.voilio.global.common.Category;
+import lombok.*;
+
+import javax.persistence.*;
+import java.net.URL;
+
+@Entity
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Board extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @Column
+    private int userId;
+
+    @NotNull
+    @Column
+    private String title;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Category category1;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Category category2;
+
+    @Column
+    private String content;
+
+    @Column
+    private String video;
+
+    @Column
+    private String thumbnail;
+
+    @Column
+    private boolean isPublic;
 }
