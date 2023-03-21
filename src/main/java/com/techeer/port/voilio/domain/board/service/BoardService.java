aaa28040ce38e3b6e5e60d6cec837db16e402941
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-03-21 20:02:57 +0900

    feat: deleteBoard Service계층 Exception 적용

--- src/main/java/com/techeer/port/voilio/domain/board/service/BoardService.java
+++ src/main/java/com/techeer/port/voilio/domain/board/service/BoardService.java
@@ -2,6 +2,7 @@ package com.techeer.port.voilio.domain.board.service;
 
 import com.techeer.port.voilio.domain.board.entity.Board;
 import com.techeer.port.voilio.domain.board.repository.BoardRepository;
+import com.techeer.port.voilio.global.common.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,7 @@ public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
     public void deleteBoard(int boardId) {
-        Board board = boardRepository.findById(boardId).orElseThrow();
+        Board board = boardRepository.findById(boardId).orElseThrow(()-> new NotFoundException("Board not found"));
         board.changeDeleted();
         boardRepository.save(board);
     }
