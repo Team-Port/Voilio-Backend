8360f5ac9616d139461ce04eda6e949a788e5020
hangyeol <snake011128@naver.com>
2023-03-21 21:08:55 +0900

    [#3]Feat: Category Enum 생성

---
+++ src/main/java/com/techeer/port/voilio/global/common/Category.java
@@ -0,0 +1,11 @@
+package com.techeer.port.voilio.global.common;
+
+public enum Category {
+    IT(1);
+
+    private final int value;
+    Category(int value) {
+        this.value = value;
+    }
+
+}
