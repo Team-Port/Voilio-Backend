80be7f1d540af546f395eec255e9d373ae651332
hangyeol <snake011128@naver.com>
2023-03-22 14:51:08 +0900

    [#8]Feat: Board 엔티티 추가

--- src/main/java/com/techeer/port/voilio/domain/board/entity/Board.java
+++ src/main/java/com/techeer/port/voilio/domain/board/entity/Board.java
@@ -1,3 +1,60 @@
 package com.techeer.port.voilio.domain.board.entity;
 
-public class Board {}
+import com.techeer.port.voilio.global.common.BaseEntity;
+import com.techeer.port.voilio.global.common.Category;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "boards")
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer board_id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Category category1;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Category category2;
+
+    @Column
+    private String video_url;
+
+    @Column
+    private String thumbnail_url;
+
+    @Column
+    private Boolean isPublic;
+
+    @Builder
+    public Board(Integer board_id, String title, String content, Category category1,
+        Category category2, String video_url, String thumbnail_url, boolean isPublic) {
+        this.board_id = board_id;
+        this.title = title;
+        this.category1 = category1;
+        this.category2 = category2;
+        this.content = content;
+        this.video_url = video_url;
+        this.thumbnail_url = thumbnail_url;
+        this.isPublic = isPublic;
+    }
+}
+
