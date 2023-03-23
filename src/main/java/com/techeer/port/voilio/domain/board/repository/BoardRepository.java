f2335fbbbe84fb6148612652e00d4d1a354de564
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-03-23 20:53:32 +0900

    [#12]Feat: findBoardById Repository구현

--- src/main/java/com/techeer/port/voilio/domain/board/repository/BoardRepository.java
+++ src/main/java/com/techeer/port/voilio/domain/board/repository/BoardRepository.java
@@ -4,5 +4,11 @@ import com.techeer.port.voilio.domain.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Locale;
+import java.util.Optional;
+
 @Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {}
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    Optional<Board> findByIdAndIsDeletedFalseAndIsPublicTrue(Long boardId);
+}
--- src/test/java/com/techeer/port/voilio/domain/board/repository/BoardRepositoryTest.java
+++ src/test/java/com/techeer/port/voilio/domain/board/repository/BoardRepositoryTest.java
@@ -1,33 +1,53 @@
 package com.techeer.port.voilio.domain.board.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.techeer.port.voilio.domain.board.entity.Board;
+import com.techeer.port.voilio.global.common.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-// @ExtendWith(SpringExtension.class)
-// @ActiveProfiles("test")
-// @DataJpaTest
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.internal.matchers.text.ValuePrinter.print;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
 public class BoardRepositoryTest {
-  //    @Autowired
-  //    private BoardRepository boardRepository;
-  //
-  //    @BeforeEach
-  //    public void saveTest(){
-  //        Board board1 = Board.builder()
-  //                .userId(1)
-  //                .title("testTitle")
-  //                .content("testContent")
-  //                .video("https://www.naver.com/")
-  //                .thumbnail("https://www.naver.com")
-  //        .build();
-  //        Board board2 = Board.builder()
-  //                .userId(1)
-  //                .title("testTitle2")
-  //                .content("testContent2")
-  //                .video("https://www.naver.com/")
-  //                .thumbnail("https://www.naver.com")
-  //                .build();
-  //        boardRepository.save(board1);
-  //        boardRepository.save(board2);
-  //    }
+      @Autowired
+      private BoardRepository boardRepository;
+
+      private Board board1;
+      private Board board2;
+
+      @BeforeEach
+      public void setUp(){
+          board1 = Board.builder()
+                  .userId(1L)
+                  .title("testTitle1")
+                  .content("testContent1")
+                  .isPublic(true)
+                  .category1(Category.IT)
+                  .video("https://www.naver.com/")
+                  .thumbnail("https://www.naver.com")
+          .build();
+          board2 = Board.builder()
+                  .userId(1L)
+                  .title("testTitle2")
+                  .category1(Category.IT)
+                  .content("testContent2")
+                  .isPublic(false)
+                  .video("https://www.naver.com/")
+                  .thumbnail("https://www.naver.com")
+                  .build();
+
+          board1 = boardRepository.save(board1);
+          board2 = boardRepository.save(board2);
+      }
   //
   //    @Test
   //    @DisplayName("deleteBoardTest Start")
@@ -48,4 +68,25 @@ public class BoardRepositoryTest {
   //        assertTrue(board.getIsDeleted());
   //
   //    }
+
+     @Test
+     @DisplayName("testFindBoardByBoardId_when_existedBoard")
+     public void testFindBoardByBoardId(){
+          //given
+          Board existedBoard = board1;
+          Board existedBoard2 = board2;
+
+          //when
+         Board foundBoard1 = boardRepository.findByIdAndIsDeletedFalseAndIsPublicTrue(existedBoard.getId()).orElseThrow();
+//         Board foundBoard2 = boardRepository.fin
+
+         //         System.out.println(foundBoard);
+
+         //then
+         assertEquals(foundBoard1.getId(),existedBoard.getId());
+         assertTrue(foundBoard1.getIsPublic());
+         assertFalse(foundBoard1.getIsDeleted());
+
+     }
+
 }
