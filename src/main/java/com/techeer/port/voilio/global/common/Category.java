15e93f6e49cfe1050e21f1fc513ee1486d900dec
hangyeol <snake011128@naver.com>
2023-03-23 11:54:39 +0900

    [#3]Feat: Category에 @Getter, @AllArgsConstructor 추가

--- src/main/java/com/techeer/port/voilio/global/common/Category.java
+++ src/main/java/com/techeer/port/voilio/global/common/Category.java
@@ -1,11 +1,12 @@
 package com.techeer.port.voilio.global.common;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Category {
   IT(1);
 
   private final int value;
-
-  Category(int value) {
-    this.value = value;
-  }
 }
