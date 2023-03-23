2b48ccc9f6586909b5bc971e23bd6b46c49eff85
hangyeol <snake011128@naver.com>
2023-03-23 12:04:07 +0900

    [#3]Feat: Column에 @NotNull 적용

--- src/main/java/com/techeer/port/voilio/domain/board/entity/Board.java
+++ src/main/java/com/techeer/port/voilio/domain/board/entity/Board.java
@@ -2,6 +2,7 @@ package com.techeer.port.voilio.domain.board.entity;
 
 import com.techeer.port.voilio.global.common.Category;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,45 +12,57 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Board {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Integer board_id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer board_id;
 
-  @Column private String title;
+    @Column
+    @NotNull
+    private String title;
 
-  @Column private String content;
+    @Column
+    @NotNull
+    private String content;
 
-  @Column
-  @Enumerated(EnumType.STRING)
-  private Category category1;
+    @Column
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Category category1;
 
-  @Column
-  @Enumerated(EnumType.STRING)
-  private Category category2;
+    @Column
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Category category2;
 
-  @Column private String video_url;
+    @Column
+    @NotNull
+    private String video_url;
 
-  @Column private String thumbnail_url;
+    @Column
+    @NotNull
+    private String thumbnail_url;
 
-  @Column private Boolean isPublic;
+    @Column
+    @NotNull
+    private Boolean isPublic;
 
-  @Builder
-  public Board(
-      Integer board_id,
-      String title,
-      String content,
-      Category category1,
-      Category category2,
-      String video_url,
-      String thumbnail_url,
-      boolean isPublic) {
-    this.board_id = board_id;
-    this.title = title;
-    this.category1 = category1;
-    this.category2 = category2;
-    this.content = content;
-    this.video_url = video_url;
-    this.thumbnail_url = thumbnail_url;
-    this.isPublic = isPublic;
-  }
+    @Builder
+    public Board(
+        Integer board_id,
+        String title,
+        String content,
+        Category category1,
+        Category category2,
+        String video_url,
+        String thumbnail_url,
+        boolean isPublic) {
+        this.board_id = board_id;
+        this.title = title;
+        this.category1 = category1;
+        this.category2 = category2;
+        this.content = content;
+        this.video_url = video_url;
+        this.thumbnail_url = thumbnail_url;
+        this.isPublic = isPublic;
+    }
 }
