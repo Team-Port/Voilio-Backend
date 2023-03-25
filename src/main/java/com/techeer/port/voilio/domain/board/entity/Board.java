6d0e735c629518727b148cf4cb8af8f3ed104e3f
hangyeol <snake011128@naver.com>
2023-03-25 17:48:18 +0900

    [#17]Feat: 수정할때 필요한 setBoard 작성

--- src/main/java/com/techeer/port/voilio/domain/board/entity/Board.java
+++ src/main/java/com/techeer/port/voilio/domain/board/entity/Board.java
@@ -20,28 +20,36 @@ import lombok.ToString;
 @Table(name = "boards")
 public class Board extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "board_id")
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
 
-  @Column @NotNull private String title;
+    @Column
+    @NotNull
+    private String title;
 
-  @Column @NotNull private String content;
+    @Column
+    @NotNull
+    private String content;
 
-  @Column
-  @NotNull
-  @Enumerated(EnumType.STRING)
-  private Category category1;
+    @Column
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Category category1;
 
-  @Column
-  @NotNull
-  @Enumerated(EnumType.STRING)
-  private Category category2;
+    @Column
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Category category2;
 
-  @Column @NotNull private String video_url;
+    @Column
+    @NotNull
+    private String video_url;
 
-  @Column @NotNull private String thumbnail_url;
+    @Column
+    @NotNull
+    private String thumbnail_url;
 
   @Column(columnDefinition = "boolean default true")
   @NotNull
@@ -52,8 +60,8 @@ public class Board extends BaseEntity {
   //  @NotNull
   //  private User user;
 
-  @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
-  private List<Comment> comments = new ArrayList<>();
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 
   @Builder
   public Board(
