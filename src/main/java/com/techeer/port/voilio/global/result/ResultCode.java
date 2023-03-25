a7439338ee114ceff71aeeafd8d7fa40ef7ddd74
hangyeol <snake011128@naver.com>
2023-03-25 17:43:11 +0900

    [#17]Feat: 게시물 수정 ResultCode 추가

--- src/main/java/com/techeer/port/voilio/global/result/ResultCode.java
+++ src/main/java/com/techeer/port/voilio/global/result/ResultCode.java
@@ -13,6 +13,7 @@ public enum ResultCode {
   USER_REGISTRATION_SUCCESS("U001", "200", "사용자 등록 성공"),
 
   BOARD_CREATED_SUCCESS("B001", "201", "게시물이 정상적으로 생성되었습니다."),
+  BOARD_UPDATED_SUCCESS("B002", "200", "게시물이 정상적으로 수정되었습니다.")
   ;
 
   private final String code;
