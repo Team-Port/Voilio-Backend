2e20259a3180bb2f26acf0d66a10dd3ef93b927b
hangyeol <snake011128@naver.com>
2023-03-28 13:35:06 +0900

    [#23]Feat: List형 옵션 추가

--- src/main/java/com/techeer/port/voilio/domain/board/repository/BoardRepository.java
+++ src/main/java/com/techeer/port/voilio/domain/board/repository/BoardRepository.java
@@ -2,10 +2,15 @@ package com.techeer.port.voilio.domain.board.repository;
 
 import com.techeer.port.voilio.domain.board.entity.Board;
 import java.util.Optional;
+import com.techeer.port.voilio.global.result.ResultResponse;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
   Optional<Board> findByIdAndIsDeleted(Long boardId, Boolean isDeleted);
+}
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findAllByIsDeletedAndIsPublic(Boolean isDeleted, Boolean isPublic);
 }
