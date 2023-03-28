23c7e6b07e66e32695cbf888532298e5900f6084
kimich <ickim1218@gmail.com>
2023-03-28 12:17:58 +0900

    [#15]Feat: comment 생성에 필요한 Request 값 구현

--- src/main/java/com/techeer/port/voilio/domain/comment/dto/request/CommentRequest.java
+++ src/main/java/com/techeer/port/voilio/domain/comment/dto/request/CommentRequest.java
@@ -1,3 +1,16 @@
 package com.techeer.port.voilio.domain.comment.dto.request;
 
-public class CommentRequest {}
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class CommentRequest {
+
+  @Schema(description = "게시판 아이디") @NotNull private Long boardId;
+  @Schema(description = "댓글 내용") @NotNull private String content;
+}
