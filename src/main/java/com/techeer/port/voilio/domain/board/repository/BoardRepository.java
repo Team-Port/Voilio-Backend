eaff95129adf82302184bae8a612e5ee761d29a8
hangyeol <snake011128@naver.com>
2023-03-29 20:57:07 +0900

    [#23]Feat: Repo에 findAllByIsDeletedAndIsPublic 추가

--- src/main/java/com/techeer/port/voilio/domain/board/repository/BoardRepository.java
+++ src/main/java/com/techeer/port/voilio/domain/board/repository/BoardRepository.java
@@ -4,6 +4,8 @@ import com.techeer.port.voilio.domain.board.entity.Board;
 import java.util.Optional;
 import com.techeer.port.voilio.global.result.ResultResponse;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,6 @@ import org.springframework.stereotype.Repository;
 public interface BoardRepository extends JpaRepository<Board, Long> {
   Optional<Board> findByIdAndIsDeleted(Long boardId, Boolean isDeleted);
   List<Board> findAllByIsDeletedAndIsPublic(Boolean isDeleted, Boolean isPublic);
+  Optional<Board> findByIdAndIsDeletedFalseAndIsPublicTrue(Long boardId);
+  Page<Board> findAllByIsDeletedAndIsPublic(Boolean isDeleted, Boolean isPublic, Pageable pageable);
 }
