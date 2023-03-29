82b66b54599908774c28155fc1e8e2f2b68858fc
hangyeol <snake011128@naver.com>
2023-03-29 21:01:32 +0900

    [#23]Feat: 전체 게시물 출력 Controller 작성, Pagination추가

--- src/main/java/com/techeer/port/voilio/domain/board/controller/BoardController.java
+++ src/main/java/com/techeer/port/voilio/domain/board/controller/BoardController.java
@@ -12,12 +12,16 @@ import com.techeer.port.voilio.domain.board.dto.request.BoardCreateRequest;
 import com.techeer.port.voilio.domain.board.dto.request.BoardUpdateRequest;
 import com.techeer.port.voilio.domain.board.dto.response.BoardResponse;
 import com.techeer.port.voilio.domain.board.entity.Board;
+import com.techeer.port.voilio.domain.board.mapper.BoardMapper;
 import com.techeer.port.voilio.domain.board.service.BoardService;
 import com.techeer.port.voilio.global.result.ResultResponse;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -49,43 +53,38 @@ public class BoardController {
   }
 
   @PostMapping("/create")
-  public ResponseEntity<ResultResponse> createBoard(
-      @Validated @RequestBody BoardCreateRequest request) {
+  public ResponseEntity<ResultResponse> createBoard(@Validated @RequestBody BoardRequest request) {
     boardService.createBoard(request);
     ResultResponse<Board> resultResponse = new ResultResponse<>(BOARD_CREATED_SUCCESS);
     resultResponse.add(linkTo(methodOn(BoardController.class).createBoard(request)).withSelfRel());
     return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
   }
 
-  @PatchMapping("{boardId}/hide")
-  public ResponseEntity<ResultResponse> hideBoard(@PathVariable Long boardId) {
-    boardService.hideBoard(boardId);
-    ResultResponse<?> resultResponse = new ResultResponse<>(BOARD_UPDATED_SUCCESS);
-    resultResponse.add(linkTo(methodOn(BoardController.class).hideBoard(boardId)).withSelfRel());
-    return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
-  }
+    @GetMapping("/list")
+    public ResponseEntity<PagedModel<EntityModel<BoardResponse>>> findAllBoard(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        Page<Board> boardPage = boardService.findAllBoard(PageRequest.of(page, size));
+        List<EntityModel<BoardResponse>> boardModels = boardPage.getContent().stream()
+            .map(board -> EntityModel.of(
+                boardMapper.toDto(board),
+                linkTo(methodOn(BoardController.class).findBoardById(board.getId())).withSelfRel()))
 
-  @GetMapping
-  public ResponseEntity<ResultResponse<List<EntityModel<BoardResponse>>>> findBoardByKeyword(
-      @RequestParam("search") String search) {
-    List<EntityModel<BoardResponse>> boards =
-        boardService.findBoardByKeyword(search).stream()
-            .map(
-                board ->
-                    EntityModel.of(
-                        board,
-                        linkTo(methodOn(BoardController.class).findBoardById(board.getId()))
-                            .withSelfRel()))
-            .collect(Collectors.toList());
+        PagedModel.PageMetadata metadata = new PagedModel.PageMetadata(
+            boardPage.getSize(),
+            boardPage.getTotalElements(),
+            boardPage.getTotalPages());
+        PagedModel<EntityModel<BoardResponse>> result = PagedModel.of(
+            boardModels, metadata,
+            linkTo(methodOn(BoardController.class).findAllBoard(page, size)).withSelfRel());
 
-    ResultResponse<List<EntityModel<BoardResponse>>> resultResponse =
-        new ResultResponse<>(BOARD_FIND_SUCCESS, boards);
+        ResultResponse<PagedModel<EntityModel<BoardResponse>>> resultResponse = new ResultResponse<>(BOARD_FINDALL_SUCCESS, result);
+        resultResponse.add(linkTo(BoardController.class).slash("list").withSelfRel());
+        return ResponseEntity.ok().body(result);
 
-    resultResponse.add(
-        linkTo(methodOn(BoardController.class).findBoardByKeyword(search)).withSelfRel());
-    return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
-  }
 
+    }
   @PutMapping("/update/{boardId}")
   public ResponseEntity<ResultResponse> updateBoard(
       @PathVariable Long boardId, @RequestBody BoardUpdateRequest request) {
@@ -95,16 +94,4 @@ public class BoardController {
         linkTo(methodOn(BoardController.class).updateBoard(boardId, request)).withSelfRel());
     return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
   }
-    @GetMapping("/list")
-        List<Board> boardList = boardService.findAllBoard();
-    public BoardResponse getAllBoards() {
-        List<BoardResponse.BoardData> boardDataList = boardList.stream()
-            .map(board -> new BoardResponse.BoardData(board.getId(), board.getTitle(),
-                board.getContent(),
-                board.getCategory1(), board.getCategory2(), board.getThumbnail_url()))
-            .collect(Collectors.toList());
-        BoardResponse response = new BoardResponse(boardDataList);
-        return response;
-        response.add(linkTo(methodOn(BoardController.class).getAllBoards()).withSelfRel());
-    }
 }
