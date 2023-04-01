94b41d253f3118e08471109b35697f5165a938a2
hangyeol <snake011128@naver.com>
2023-04-01 19:55:53 +0900

    [#23]Fix: Size Default 30으로 변경

--- src/main/java/com/techeer/port/voilio/domain/board/controller/BoardController.java
+++ src/main/java/com/techeer/port/voilio/domain/board/controller/BoardController.java
@@ -77,7 +77,7 @@ public class BoardController {
     @GetMapping("/list")
     public ResponseEntity<PagedModel<EntityModel<BoardResponse>>> findAllBoard(
         @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "10") int size
+        @RequestParam(defaultValue = "30") int size
     ) {
         Page<Board> boardPage = boardService.findAllBoard(PageRequest.of(page, size));
         List<EntityModel<BoardResponse>> boardModels = boardPage.getContent().stream()
@@ -97,7 +97,5 @@ public class BoardController {
         ResultResponse<PagedModel<EntityModel<BoardResponse>>> resultResponse = new ResultResponse<>(BOARD_FINDALL_SUCCESS, result);
         resultResponse.add(linkTo(BoardController.class).slash("list").withSelfRel());
         return ResponseEntity.ok().body(result);
-
-
     }
 }
