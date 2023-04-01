7e765995d8ae14e191f886acfccfc5d8f5f3861c
kimich <ickim1218@gmail.com>
2023-04-01 11:39:47 +0900

    [#27]Feat: 댓글 정보 클래스 구현

---
+++ src/main/java/com/techeer/port/voilio/domain/comment/dto/request/CommentInfo.java
@@ -0,0 +1,26 @@
+package com.techeer.port.voilio.domain.comment.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class CommentInfo {
+
+  private String content;
+
+  @DateTimeFormat private LocalDateTime createAt;
+
+  @DateTimeFormat private LocalDateTime updateAt;
+
+  @Builder
+  public CommentInfo(String content, LocalDateTime createAt, LocalDateTime updateAt){
+    this.content = content;
+    this.createAt = createAt;
+    this.updateAt = updateAt;
+}
+}
