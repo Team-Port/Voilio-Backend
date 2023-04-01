71bbde9d87f58cc0641fa2bc5fed1f5210944b1b
kimich <ickim1218@gmail.com>
2023-04-01 11:41:24 +0900

    [#27]Feat: 댓글을 수정할 수 있도록 Service 구현

--- src/main/java/com/techeer/port/voilio/domain/comment/service/CommentService.java
+++ src/main/java/com/techeer/port/voilio/domain/comment/service/CommentService.java
@@ -2,8 +2,11 @@ package com.techeer.port.voilio.domain.comment.service;
 
 import com.techeer.port.voilio.domain.board.entity.Board;
 import com.techeer.port.voilio.domain.board.repository.BoardRepository;
+import com.techeer.port.voilio.domain.comment.dto.request.CommentInfo;
 import com.techeer.port.voilio.domain.comment.dto.request.CommentRequest;
+import com.techeer.port.voilio.domain.comment.dto.request.CommentUpdateRequest;
 import com.techeer.port.voilio.domain.comment.entity.Comment;
+import com.techeer.port.voilio.domain.comment.exception.NotFoundCommentException;
 import com.techeer.port.voilio.domain.comment.repository.CommentRepository;
 import com.techeer.port.voilio.domain.user.exception.NotFoundUserException;
 import lombok.RequiredArgsConstructor;
@@ -16,22 +19,41 @@ public class CommentService {
 
   private final BoardRepository boardRepository;
 
-  public Comment registerComment(CommentRequest commentRequest) {
+  public CommentInfo registerComment(CommentRequest commentRequest) {
 
     Comment createNewCommentEntity = CreateNewCommentEntity(commentRequest);
     Comment comment = commentRepository.save(createNewCommentEntity);
 
-    return comment;
+    return commentEntityToCommentInfo(comment);
   }
 
-  public Comment CreateNewCommentEntity(CommentRequest commentRequest) {
+  public CommentInfo updateComment(CommentUpdateRequest commentUpdateRequest, Long commentId) {
+    Comment foundComment = findCommentById(commentId);
+    foundComment.updateComment(commentUpdateRequest);
+
+    Comment savedComment = commentRepository.save(foundComment);
+    return commentEntityToCommentInfo(savedComment);
+  }
+
+  private CommentInfo commentEntityToCommentInfo(Comment savedComment) {
+    return CommentInfo.builder()
+        .content(savedComment.getContent())
+        .createAt(savedComment.getCreateAt())
+        .updateAt(savedComment.getUpdateAt())
+        .build();
+  }
+
+  private Comment CreateNewCommentEntity(CommentRequest commentRequest) {
 
     Board findBoard = getBoardById(commentRequest.getBoardId());
-
     return Comment.builder().board(findBoard).content(commentRequest.getContent()).build();
   }
 
   private Board getBoardById(Long boardId) {
     return boardRepository.findById(boardId).orElseThrow(NotFoundUserException::new);
+  }
+
+  private Comment findCommentById(Long id) {
+    return commentRepository.findById(id).orElseThrow(NotFoundCommentException::new);
   }
 }
