7caf1e5ad07ad80ed87c32de76c089894f2d5578
gilyeon00 <12venus08@naver.com>
2023-04-01 20:41:01 +0900

    [#28]Feat: 비밀번호 Setter 구현 - Set encoded 비밀번호

--- src/main/java/com/techeer/port/voilio/domain/user/dto/request/UserRequest.java
+++ src/main/java/com/techeer/port/voilio/domain/user/dto/request/UserRequest.java
@@ -1,12 +1,7 @@
 package com.techeer.port.voilio.domain.user.dto.request;
 
 import com.techeer.port.voilio.domain.user.entity.User;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
-import javax.validation.constraints.NotNull;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-//import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Builder
@@ -14,18 +9,24 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class UserRequest {
+    
     private Long id;
     private String email;
     private String password;
     private String nickname;
 
     public User toEntity(){
+
         return User.builder()
                 .id(id)
                 .email(email)
-//                .password(new BCryptPasswordEncoder().encode(password))
                 .password(password)
                 .nickname(nickname)
                 .build();
     }
+
+    public void setUserPassword(String encodedPassword) {
+        this.password = encodedPassword;
+    }
+
 }
