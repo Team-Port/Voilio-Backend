67e860ad1cd3a2b8e2057ee87d541118ee325866
gilyeon00 <12venus08@naver.com>
2023-04-01 19:42:01 +0900

    [#28]Feat: User service 구현 - registerUser 생성

--- src/main/java/com/techeer/port/voilio/domain/user/service/UserService.java
+++ src/main/java/com/techeer/port/voilio/domain/user/service/UserService.java
@@ -1,3 +1,22 @@
 package com.techeer.port.voilio.domain.user.service;
 
-public class UserService {}
+import com.techeer.port.voilio.domain.user.dto.request.UserRequest;
+import com.techeer.port.voilio.domain.user.entity.User;
+import com.techeer.port.voilio.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public void registerUser(UserRequest userRequest) {
+        User user = userRequest.toEntity();
+        userRepository.save(user);
+    }
+
+
+}
