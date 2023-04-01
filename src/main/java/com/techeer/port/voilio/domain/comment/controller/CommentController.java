92c42f1fef9774b846c6e08080c40e318fa79869
kimich <ickim1218@gmail.com>
2023-04-01 11:41:55 +0900

    [#27]Feat: 댓글 수정 Controller 구현

--- src/main/java/com/techeer/port/voilio/domain/comment/controller/CommentController.java
+++ src/main/java/com/techeer/port/voilio/domain/comment/controller/CommentController.java
@@ -1,29 +1,27 @@
 package com.techeer.port.voilio.domain.comment.controller;
 
 import static com.techeer.port.voilio.global.result.ResultCode.COMMENT_CREATED_SUCCESS;
+import static com.techeer.port.voilio.global.result.ResultCode.UPDATE_COMMENT_SUCCESS;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import com.techeer.port.voilio.domain.comment.dto.request.CommentRequest;
+import com.techeer.port.voilio.domain.comment.dto.request.CommentUpdateRequest;
 import com.techeer.port.voilio.domain.comment.entity.Comment;
 import com.techeer.port.voilio.domain.comment.service.CommentService;
 import com.techeer.port.voilio.global.result.ResultResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
-import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Comment", description = "Comment API Document")
 @RequestMapping("/api/v1/comments")
 @RestController
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public class CommentController {
 
   private final CommentService commentService;
@@ -38,6 +36,21 @@ public class CommentController {
     ResultResponse<Comment> resultResponse = new ResultResponse<>(COMMENT_CREATED_SUCCESS);
     resultResponse.add(
         linkTo(methodOn(CommentController.class).createComment(commentRequest)).withSelfRel());
+
+    return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
+  }
+
+  @Operation(summary = "댓글 수정", description = "댓글 수정 메서드입니다.")
+  @PutMapping("/{commentId}")
+  public ResponseEntity<ResultResponse> updateComment(
+      @Valid @RequestBody CommentUpdateRequest commentUpdateRequest, @PathVariable Long commentId) {
+
+    commentService.updateComment(commentUpdateRequest, commentId);
+
+    ResultResponse<Comment> resultResponse = new ResultResponse<>(UPDATE_COMMENT_SUCCESS);
+    resultResponse.add(
+        linkTo(methodOn(CommentController.class).updateComment(commentUpdateRequest, commentId))
+            .withSelfRel());
 
     return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
   }
