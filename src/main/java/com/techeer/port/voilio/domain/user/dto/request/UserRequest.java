a91bbec660ce449e94cc85c94d11e2647be7a2ad
gilyeon00 <12venus08@naver.com>
2023-04-01 19:41:26 +0900

    [#28]Feat: User request 구현 - Spring Security 추후 적용 예정

--- src/main/java/com/techeer/port/voilio/domain/user/dto/request/UserRequest.java
+++ src/main/java/com/techeer/port/voilio/domain/user/dto/request/UserRequest.java
@@ -1,3 +1,31 @@
 package com.techeer.port.voilio.domain.user.dto.request;
 
-public class UserRequest {}
+import com.techeer.port.voilio.domain.user.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import javax.validation.constraints.NotNull;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class UserRequest {
+    private Long id;
+    private String email;
+    private String password;
+    private String nickname;
+
+    public User toEntity(){
+        return User.builder()
+                .id(id)
+                .email(email)
+//                .password(new BCryptPasswordEncoder().encode(password))
+                .password(password)
+                .nickname(nickname)
+                .build();
+    }
+}
