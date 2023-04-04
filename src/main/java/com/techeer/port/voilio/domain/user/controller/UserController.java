e85698539e73181dc8bc3f431123b9af7e3466c9
gilyeon00 <12venus08@naver.com>
2023-04-04 11:00:28 +0900

    [#31]Feat: 전체 User List Controller 구현 - 각각의 회원에 대한 HATEOAS 사용 - HATEOAS : REST API의 규약 중 "어플리케이션 상태에 대한 엔진으로써 하이퍼미디어"

--- src/main/java/com/techeer/port/voilio/domain/user/controller/UserController.java
+++ src/main/java/com/techeer/port/voilio/domain/user/controller/UserController.java
@@ -12,9 +12,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Tag(name = "User", description = "User API Document")
 @RequestMapping("api/v1/users")
@@ -34,5 +39,15 @@ public class UserController {
         linkTo(methodOn(UserController.class).createUser(userRequest)).withSelfRel());
 
     return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
+  }
+
+  @GetMapping("/list")
+  @Operation(summary = "회원 출력", description = "전체 회원 출력 메서드입니다.")
+  public CollectionModel<EntityModel<User>> getUserList(){
+    List<EntityModel<User>> users = userService.getUserList().stream()
+            .map(user -> EntityModel.of(user,
+                    linkTo(methodOn(UserController.class).getUserList()).withSelfRel()))
+            .collect(Collectors.toList());
+    return CollectionModel.of(users, linkTo(methodOn(UserController.class).getUserList()).withSelfRel());
   }
 }
