981f5d645079ea0a7fe622a327da8a0df632b5c0
kimich <ickim1218@gmail.com>
2023-04-05 21:53:25 +0900

    [#35]Feat: 게시판 아이디로 댓글 조회 구현

--- src/main/java/com/techeer/port/voilio/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/techeer/port/voilio/domain/comment/repository/CommentRepository.java
@@ -2,5 +2,11 @@ package com.techeer.port.voilio.domain.comment.repository;
 
 import com.techeer.port.voilio.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;import org.springframework.data.repository.query.Param;
+import java.util.List;import java.util.Optional;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {}
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+  @Query("SELECT c FROM Comment c join fetch c.board where c.isDeleted = false and c.board.id=:id")
+  List<Comment> findByBoardId(@Param("id") Long id);
+}
