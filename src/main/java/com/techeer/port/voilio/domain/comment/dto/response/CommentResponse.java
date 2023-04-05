6056c79397e9c2d6e45ec3dea6636ae1ccd6481e
kimich <ickim1218@gmail.com>
2023-04-05 21:46:24 +0900

    [#35]Feat: 댓글 조회 Response 구현 및 CommentRequest 값에 userId 추가

--- src/main/java/com/techeer/port/voilio/domain/comment/dto/response/CommentResponse.java
+++ src/main/java/com/techeer/port/voilio/domain/comment/dto/response/CommentResponse.java
@@ -1,3 +1,18 @@
 package com.techeer.port.voilio.domain.comment.dto.response;
 
-public class CommentResponse {}
+import com.techeer.port.voilio.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CommentResponse {
+
+  private Long commentId;
+  private String nickname;
+  private String content;
+  private LocalDateTime localDateTime;
+}
