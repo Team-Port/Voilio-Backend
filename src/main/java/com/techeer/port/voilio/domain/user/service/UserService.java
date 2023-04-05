382ce4005df708b4e33060af5abf311541edda4b
gilyeon00 <12venus08@naver.com>
2023-04-06 01:13:58 +0900

    [#36]Feat: id를 통해 지정 User 가져오는 Service 로직 구현

--- src/main/java/com/techeer/port/voilio/domain/user/service/UserService.java
+++ src/main/java/com/techeer/port/voilio/domain/user/service/UserService.java
@@ -1,7 +1,12 @@
 package com.techeer.port.voilio.domain.user.service;
 
+import com.techeer.port.voilio.domain.board.dto.response.BoardResponse;
+import com.techeer.port.voilio.domain.board.entity.Board;
+import com.techeer.port.voilio.domain.board.exception.NotFoundBoard;
+import com.techeer.port.voilio.domain.board.exception.NotFoundUser;
 import com.techeer.port.voilio.domain.user.dto.request.UserRequest;
 import com.techeer.port.voilio.domain.user.entity.User;
+import com.techeer.port.voilio.domain.user.exception.NotFoundUserException;
 import com.techeer.port.voilio.domain.user.repository.UserRepository;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,4 +32,13 @@ public class UserService {
   public List<User> getUserList() {
     return new ArrayList<User>(userRepository.findAll());
   }
+
+  public User getUserById (Long user_id) {
+    User user =
+            userRepository
+                    .findUserById(user_id)
+                    .orElseThrow(NotFoundUser::new);
+    return user;
+  }
+
 }
