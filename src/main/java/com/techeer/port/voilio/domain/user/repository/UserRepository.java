e56afc07d8165d5eaad8e9c1aa57a9b9b4e294ed
gilyeon00 <12venus08@naver.com>
2023-04-06 01:14:50 +0900

    [#36]Feat: delete 되지 않은 사용자 select query문 통해 findUserById() 구현

--- src/main/java/com/techeer/port/voilio/domain/user/repository/UserRepository.java
+++ src/main/java/com/techeer/port/voilio/domain/user/repository/UserRepository.java
@@ -2,7 +2,16 @@ package com.techeer.port.voilio.domain.user.repository;
 
 import com.techeer.port.voilio.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.id = :id and u.isDeleted=false ")
+    Optional<User> findUserById(@Param("id") long id);
+
+}
