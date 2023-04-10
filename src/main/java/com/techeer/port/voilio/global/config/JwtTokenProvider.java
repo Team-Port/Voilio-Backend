f0961f0b81ac7a5b0c8e934b876c59fb6d78979e
gilyeon00 <12venus08@naver.com>
2023-04-10 13:08:04 +0900

    [#56]Feat: getAuthentication, validateToken 구현

--- src/main/java/com/techeer/port/voilio/global/config/JwtTokenProvider.java
+++ src/main/java/com/techeer/port/voilio/global/config/JwtTokenProvider.java
@@ -1,29 +1,37 @@
 package com.techeer.port.voilio.global.config;
 
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
+
 import javax.xml.bind.DatatypeConverter;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Component
-public class JwtTokenProvider{
-    private final SecretKey key;
+public class JwtTokenProvider {
+    private final Key key;
 
-    public  JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
+    public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
         byte[] secretByteKey = DatatypeConverter.parseBase64Binary(secretKey);
         this.key = Keys.hmacShaKeyFor(secretByteKey);
-
     }
 
     public JwtToken generateToken(Authentication authentication) {
@@ -39,9 +47,9 @@ public class JwtTokenProvider{
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
 
-        // Refrest Token 생성
+        // Refresh Token 생성
         String refreshToken = Jwts.builder()
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 *60 *60 *36))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 36))
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
 
@@ -51,5 +59,45 @@ public class JwtTokenProvider{
                 .refreshToken(refreshToken)
                 .build();
     }
+
+    public Authentication getAuthentication(String accessToken) {
+        // 토큰 복호화
+        Claims claims = parseClaime(accessToken);
+        if (claims.get("auth") == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get("auth").toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty", e);
+        }
+        return false;
+    }
+
+    private Claims parseClaime(String accessToken) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
 
 }
