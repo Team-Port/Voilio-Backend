9bceadd6924749876e2a2173a62094881fa6bf7c
gilyeon00 <12venus08@naver.com>
2023-04-10 13:09:06 +0900

    [#56]Feat: add httpBasic().disable() to Spring Security

--- src/main/java/com/techeer/port/voilio/global/config/SecurityConfig.java
+++ src/main/java/com/techeer/port/voilio/global/config/SecurityConfig.java
@@ -5,23 +5,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.cors()
-        .disable() // cors 방지
-        .csrf()
-        .disable() // csrf 방지
-        .formLogin()
-        .disable() // 기본 로그인 페이지 없애기
-        .headers()
-        .frameOptions()
-        .disable();
+public class SecurityConfig {
+
+  private final JwtTokenProvider jwtTokenProvider;
+
+  public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+    this.jwtTokenProvider = jwtTokenProvider;
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .formLogin().disable()
+            .httpBasic().disable()
+            .authorizeRequests()
+            .antMatchers("/api/users").permitAll()
+            .and()
+            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+            return http.build();
   }
 
   @Bean
