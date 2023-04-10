357e0fded9c154b4ac625314dc3799d145c0beb0
gilyeon00 <12venus08@naver.com>
2023-04-10 14:30:18 +0900

    [#56]Feat: login 서비스 구현 및 getUserById, getUserByEmail 통합

--- src/main/java/com/techeer/port/voilio/domain/user/service/UserService.java
+++ src/main/java/com/techeer/port/voilio/domain/user/service/UserService.java
@@ -3,18 +3,30 @@ package com.techeer.port.voilio.domain.user.service;
 import com.techeer.port.voilio.domain.board.exception.NotFoundUser;
 import com.techeer.port.voilio.domain.user.dto.request.UserRequest;
 import com.techeer.port.voilio.domain.user.entity.User;
+import com.techeer.port.voilio.domain.user.exception.InvalidPassword;
 import com.techeer.port.voilio.domain.user.repository.UserRepository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import com.techeer.port.voilio.global.config.JwtToken;
+import com.techeer.port.voilio.global.config.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserService {
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
+  private final JwtTokenProvider jwtTokenProvider;
+  private AuthenticationManagerBuilder authenticationManagerBuilder;
 
   public void registerUser(UserRequest userRequest) throws Exception {
     String password = userRequest.getPassword();
@@ -29,14 +41,38 @@ public class UserService {
     return new ArrayList<User>(userRepository.findAll());
   }
 
-  public User getUserById(Long userId) {
+  public User getUser(Long userId) {
     User user = userRepository.findUserById(userId).orElseThrow(NotFoundUser::new);
     return user;
   }
 
+  public User getUser(String email) {
+    User user = userRepository.findUserByEmail(email).orElseThrow(NotFoundUser::new);
+    return user;
+  }
+
   public void deleteUser(Long userId) {
-    User user = getUserById(userId);
+    User user = getUser(userId);
     user.changeDeleted();
     userRepository.save(user);
+  }
+
+
+  public JwtToken login(String email, String password) {
+    User user = getUser(email);
+    String encodedPwd = user.getPassword();
+
+    if(!passwordEncoder.matches(password, encodedPwd)){
+      throw new InvalidPassword();
+    }
+
+    // Authentication 객체 생성
+    UsernamePasswordAuthenticationToken authenticationToken =  new UsernamePasswordAuthenticationToken(email, password);
+    Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+    // 검증된 인증 정보로 JWT 토큰 생성
+    JwtToken token = jwtTokenProvider.generateToken(authentication);
+
+    return token;
   }
 }
