c3bc8ec9a124667dcd1ca445d618e3b6f1975e09
gilyeon00 <12venus08@naver.com>
2023-04-10 19:06:13 +0900

    [#56]Feat: JwtAuthenticationFilter 구현

--- src/main/java/com/techeer/port/voilio/global/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/techeer/port/voilio/global/config/security/JwtAuthenticationFilter.java
@@ -1,42 +1,34 @@
 package com.techeer.port.voilio.global.config.security;
 
-import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.StringUtils;
-import org.springframework.web.filter.GenericFilterBean;
+import java.io.IOException;
 
+//해당 클래스는 JwtTokenProvider가 검증을 끝낸 Jwt로부터 유저 정보를 조회해와서 UserPasswordAuthenticationFilter 로 전달합니다.
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
+
   private final JwtProvider jwtProvider;
 
-  public JwtAuthenticationFilter(JwtProvider jwtProvider) {
-    this.jwtProvider = jwtProvider;
-  }
-
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-    String token = resolveToken((HttpServletRequest) request);
-
-    // token 유효성 검사
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    // 헤더에서 JWT 를 받아옵니다.
+    String token = jwtProvider.resolveToken((HttpServletRequest) request);
+    // 유효한 토큰인지 확인합니다.
     if (token != null && jwtProvider.validateToken(token)) {
+      // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
       Authentication authentication = jwtProvider.getAuthentication(token);
+      // SecurityContext 에 Authentication 객체를 저장합니다.
       SecurityContextHolder.getContext().setAuthentication(authentication);
     }
     chain.doFilter(request, response);
-  }
-
-  // Header 에서 token 추출
-  private String resolveToken(HttpServletRequest request) {
-    String bearerToken = request.getHeader("Authorization");
-    if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
-      return bearerToken.substring(7);
-    }
-    return null;
   }
 }
