78400d2fdde59227797dcfc6f74a5b1c4e6422bf
gilyeon00 <12venus08@naver.com>
2023-04-11 15:15:54 +0900

    [#12]Feat: add resolveToken to filter chain

--- src/main/java/com/techeer/port/voilio/global/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/techeer/port/voilio/global/config/security/JwtAuthenticationFilter.java
@@ -1,34 +1,42 @@
 package com.techeer.port.voilio.global.config.security;
 
-import java.io.IOException;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-// 해당 클래스는 JwtTokenProvider가 검증을 끝낸 Jwt로부터 유저 정보를 조회해와서 UserPasswordAuthenticationFilter 로 전달합니다.
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
-
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+  public static final String AUTHORIZATION_HEADER = "Authorization";
+  public static final String BEARER_PREFIX = "Bearer ";
   private final JwtProvider jwtProvider;
 
+
+  private String resolveToken(HttpServletRequest request) {
+    String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+    if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+      return bearerToken.substring(7);
+    }
+    return null;
+  }
+
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-    // 헤더에서 JWT 를 받아옵니다.
-    String token = jwtProvider.resolveToken((HttpServletRequest) request);
-    // 유효한 토큰인지 확인합니다.
-    if (token != null && jwtProvider.validateToken(token)) {
-      // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
-      Authentication authentication = jwtProvider.getAuthentication(token);
-      // SecurityContext 에 Authentication 객체를 저장합니다.
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    String jwt = resolveToken(request);
+    // 유효한 토큰인지 확인
+    if (StringUtils.hasText(jwt) && jwtProvider.validateToken(jwt)) {
+      Authentication authentication = jwtProvider.getAuthentication(jwt);
       SecurityContextHolder.getContext().setAuthentication(authentication);
     }
-    chain.doFilter(request, response);
+
+    filterChain.doFilter(request, response);
   }
 }
