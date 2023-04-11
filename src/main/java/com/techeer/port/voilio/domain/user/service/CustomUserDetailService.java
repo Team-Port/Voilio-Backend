02fa8a926a6bda206eb75aadedd6ed5b54bfbc4e
gilyeon00 <12venus08@naver.com>
2023-04-11 15:17:59 +0900

    [#12]Feat: Custom User Detail Service 구체화

---
+++ src/main/java/com/techeer/port/voilio/domain/user/service/CustomUserDetailService.java
@@ -0,0 +1,37 @@
+package com.techeer.port.voilio.domain.user.service;
+
+import com.techeer.port.voilio.domain.user.entity.User;
+import com.techeer.port.voilio.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+  private final UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    return userRepository.findUserByEmail(email)
+            .map(this::createUserDetails)
+            .orElseThrow(() -> new UsernameNotFoundException(email + " 을 DB에서 찾을 수 없습니다"));
+  }
+
+  private UserDetails createUserDetails(User user) {
+    GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getAuthority().toString());
+
+    return new org.springframework.security.core.userdetails.User(
+            String.valueOf(user.getId()),
+            user.getPassword(),
+            Collections.singleton(grantedAuthority)
+    );
+  }
+}
