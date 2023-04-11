689d7f36a8d09767dc45fb259e90dcc3a7955ecb
gilyeon00 <12venus08@naver.com>
2023-04-11 15:18:50 +0900

    [#12]Feat: add authority to request builder

--- src/main/java/com/techeer/port/voilio/domain/user/dto/request/UserRequest.java
+++ src/main/java/com/techeer/port/voilio/domain/user/dto/request/UserRequest.java
@@ -1,8 +1,11 @@
 package com.techeer.port.voilio.domain.user.dto.request;
 
+import com.techeer.port.voilio.domain.user.entity.Authority;
 import com.techeer.port.voilio.domain.user.entity.User;
 import java.util.Collections;
 import lombok.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -15,17 +18,29 @@ public class UserRequest {
   private String nickname;
 
   public User toEntity() {
-
     return User.builder()
         .id(id)
         .email(email)
         .password(password)
         .nickname(nickname)
-        .roles(Collections.singletonList("ROLE_USER"))
+            .authority(Authority.ROLE_USER)
         .build();
+  }
+  public User toEntity(PasswordEncoder passwordEncoder) {
+    return User.builder()
+            .id(id)
+            .email(email)
+            .password(passwordEncoder.encode(password))
+            .nickname(nickname)
+            .authority(Authority.ROLE_USER)
+            .build();
   }
 
   public void setUserPassword(String encodedPassword) {
     this.password = encodedPassword;
+  }
+
+  public UsernamePasswordAuthenticationToken toAuthentication() {
+    return new UsernamePasswordAuthenticationToken(email, password);
   }
 }
