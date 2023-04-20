1a62be3a73ba0983c4dc96db98928251ca1b379d
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-04-20 16:42:43 +0900

    [#62]Feat:UploadFile관련 Exception 처리

---
+++ src/main/java/com/techeer/port/voilio/domain/board/exception/FailConvertFile.java
@@ -0,0 +1,10 @@
+package com.techeer.port.voilio.domain.board.exception;
+
+import com.techeer.port.voilio.global.error.ErrorCode;
+import com.techeer.port.voilio.global.error.exception.BusinessException;
+
+public class FailConvertFile extends BusinessException {
+    public FailConvertFile() {
+        super(ErrorCode.CONVERT_FILE_ERROR);
+    }
+}
