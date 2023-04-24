f0a4ff29db44a8261d24149553ca17de28282db4
gilyeon00 <12venus08@naver.com>
2023-04-24 22:53:36 +0900

    [#56]Feat: add Category

--- src/main/java/com/techeer/port/voilio/global/common/Category.java
+++ src/main/java/com/techeer/port/voilio/global/common/Category.java
@@ -7,7 +7,13 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Category {
   IT(1),
-  BACKEND(2);
+  BACKEND(2),
+  JAVA(3),
+  KOTLIN(4),
+  PYTHON(5),
+  REACT(6),
+  DANCE(7),
+  LANGUAGE(8);
 
   private final int value;
 }
