fe96a22bb40f96ff9b1249e7ea2cb1fc17a3982b
hangyeol <snake011128@naver.com>
2023-05-05 22:32:36 +0900

    [#70]Feat: Request, Response 추가

--- src/main/java/com/techeer/port/voilio/domain/subscribe/dto/request/SubscribeRequest.java
+++ src/main/java/com/techeer/port/voilio/domain/subscribe/dto/request/SubscribeRequest.java
@@ -1,2 +1,23 @@
-package com.techeer.port.voilio.domain.subscribe.dto.request;public class SubscribeRequest {
+package com.techeer.port.voilio.domain.subscribe.dto.request;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class SubscribeRequest {
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private Long followerId;
 }
--- src/main/java/com/techeer/port/voilio/domain/subscribe/dto/response/SubscribeResponse.java
+++ src/main/java/com/techeer/port/voilio/domain/subscribe/dto/response/SubscribeResponse.java
@@ -1,2 +1,17 @@
-package com.techeer.port.voilio.domain.subscribe.dto.response;public class SubscribeResponse {
+package com.techeer.port.voilio.domain.subscribe.dto.response;
+
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class SubscribeResponse {
+    private Long SubscribeId;
+    private String user_nickname;
+    private String follower_nickname;
+    private LocalDateTime localDateTime;
 }
