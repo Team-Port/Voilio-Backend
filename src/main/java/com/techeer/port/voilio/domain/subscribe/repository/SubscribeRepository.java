9b6370e30f5029c9d31edc473905b6c8d6aae4e2
hangyeol <snake011128@naver.com>
2023-05-05 22:32:09 +0900

    [#70]Feat: 레포지토리 추가

--- src/main/java/com/techeer/port/voilio/domain/subscribe/repository/SubscribeRepository.java
+++ src/main/java/com/techeer/port/voilio/domain/subscribe/repository/SubscribeRepository.java
@@ -1,2 +1,23 @@
-package com.techeer.port.voilio.domain.subscribe.repository;public interface SubscribeRepository {
+package com.techeer.port.voilio.domain.subscribe.repository;
+
+import com.techeer.port.voilio.domain.board.entity.Board;
+import com.techeer.port.voilio.domain.subscribe.entity.Subscribe;
+import com.techeer.port.voilio.domain.user.entity.User;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+
+    Subscribe findByUserAndFollower(User user, User follower);
+
+//    @Query("SELECT s.follower FROM Subscribe s WHERE s.user = :user")
+//    Page<Subscribe> findAllFollowers(Pageable pageable);
+
+    @Query("SELECT s FROM Subscribe s WHERE s.user.nickname = :nickname")
+    Page<Subscribe> findFollowersByNickname(@Param("nickname") String nickname, Pageable pageable);
+
 }
