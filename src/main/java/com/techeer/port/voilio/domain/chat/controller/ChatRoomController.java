4abfc8d65d29d26d1609c8db7e5e6b2b9b56d619
kimich <ickim1218@gmail.com>
2023-05-16 21:27:50 +0900

    [#72]Feat:chatting 조회 및 생성, message 수신 controller 구현

---
+++ src/main/java/com/techeer/port/voilio/domain/chat/controller/ChatRoomController.java
@@ -0,0 +1,46 @@
+package com.techeer.port.voilio.domain.chat.controller;
+
+import com.techeer.port.voilio.domain.chat.model.ChatRoom;
+import com.techeer.port.voilio.domain.chat.repo.ChatRoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/chat")
+public class ChatRoomController {
+
+  private final ChatRoomRepository chatRoomRepository;
+
+  @GetMapping("/room")
+  public String rooms(Model model) {
+    return "/chat/room";
+  }
+
+  @GetMapping("/rooms")
+  @ResponseBody
+  public List<ChatRoom> room() {
+    return chatRoomRepository.findAllRoom();
+  }
+
+  @PostMapping("/room")
+  @ResponseBody
+  public ChatRoom createRoom(@RequestParam String name) {
+    return chatRoomRepository.createChatRoom(name);
+  }
+
+  @GetMapping("/room/enter/{roomId}")
+  public String roomDetail(Model model, @PathVariable String roomId) {
+    model.addAttribute("roomId", roomId);
+    return "/chat/roomdetail";
+  }
+
+  @GetMapping("/room/{roomId}")
+  @ResponseBody
+  public ChatRoom roomInfo(@PathVariable String roomId) {
+    return chatRoomRepository.findRoomById(roomId);
+  }
+}
