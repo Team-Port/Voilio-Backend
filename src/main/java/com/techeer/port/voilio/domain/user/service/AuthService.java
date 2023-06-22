3bfda4a4bc85e42f7e630a76a9d1d9027ad256c8
gilyeon00 <12venus08@naver.com>
2023-06-23 03:25:12 +0900

    [#78]Fix: 로그인 로직에 이메일, 비밀번호 검증 로직 추가 - 기존에는 토큰 생성만 있어서, 이메일과 비밀번호가 일치하지 않아도 이메일 기준 로그인이 됐음

--- src/main/java/com/techeer/port/voilio/domain/user/service/AuthService.java
+++ src/main/java/com/techeer/port/voilio/domain/user/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.techeer.port.voilio.domain.user.service;
 
-import com.techeer.port.voilio.domain.user.dto.request.UserRequest;
+import com.techeer.port.voilio.domain.user.dto.request.UserLoginRequest;
+import com.techeer.port.voilio.domain.user.dto.request.UserSignUpRequest;
 import com.techeer.port.voilio.domain.user.dto.response.UserResponse;
 import com.techeer.port.voilio.domain.user.entity.User;
 import com.techeer.port.voilio.domain.user.repository.UserRepository;
@@ -25,22 +26,30 @@ public class AuthService {
   private final PasswordEncoder passwordEncoder;
   private final JwtProvider jwtProvider;
 
-  public UserResponse signup(UserRequest requestDto) throws AlreadyBoundException {
-    if (userRepository.existsByEmail(requestDto.getEmail())) {
+  public UserResponse signup(UserSignUpRequest userSignUpRequest) throws AlreadyBoundException {
+    if (userRepository.existsByEmail(userSignUpRequest.getEmail())) {
       throw new AlreadyBoundException();
     }
 
-    User user = requestDto.toEntity(passwordEncoder);
+    User user = userSignUpRequest.toEntity(passwordEncoder);
     return UserResponse.of(userRepository.save(user));
   }
 
-  public TokenDto login(UserRequest requestDto) {
-    UsernamePasswordAuthenticationToken authenticationToken = requestDto.toAuthentication();
-    Authentication authentication = managerBuilder.getObject().authenticate(authenticationToken);
+  public TokenDto login(UserLoginRequest userLoginRequest) {
 
-    User user = userRepository.findUserByEmail(requestDto.getEmail()).orElseThrow();
+    User user = userRepository.findUserByEmail(userLoginRequest.getEmail())
+            .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+    if (!passwordEncoder.matches(userLoginRequest.getPassword(), user.getPassword())) {
+      throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+    }
+
+    // 로그인 성공 후, 추가 작업 - 토큰 생성
     user.setActivatedAt(LocalDateTime.now());
+    UsernamePasswordAuthenticationToken authenticationToken = userLoginRequest.toAuthentication();
+    Authentication authentication = managerBuilder.getObject().authenticate(authenticationToken);
 
     return jwtProvider.generateTokenDto(authentication);
   }
+
 }
