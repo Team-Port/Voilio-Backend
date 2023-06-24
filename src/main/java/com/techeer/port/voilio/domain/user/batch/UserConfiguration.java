ccca6da3ccac8115d2b63ea7a044562dc7b58012
gilyeon00 <12venus08@naver.com>
2023-06-24 16:54:34 +0900

    [#78]Feat: userReader() 생성 (itemReader)

--- src/main/java/com/techeer/port/voilio/domain/user/batch/UserConfiguration.java
+++ src/main/java/com/techeer/port/voilio/domain/user/batch/UserConfiguration.java
@@ -8,9 +8,14 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.EntityManagerFactory;
 
 @Slf4j
 @Configuration
@@ -20,8 +25,20 @@ public class UserConfiguration {
     private final StepBuilderFactory stepBuilderFactory;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final EntityManagerFactory entityManagerFactory;
 
-    public UserConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, UserRepository userRepository, PasswordEncoder passwordEncoder, EntityManagerFactory entityManagerFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    public UserConfiguration(JobBuilderFactory jobBuilderFactory,
+                             StepBuilderFactory stepBuilderFactory,
+                             UserRepository userRepository,
+                             PasswordEncoder passwordEncoder) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
         this.userRepository = userRepository;
@@ -44,13 +61,25 @@ public class UserConfiguration {
     }
 
     @Bean
-    public Step sleeperUserStep() {
+    public Step sleeperUserStep() throws Exception {
         return this.stepBuilderFactory.get("sleeperUserStep")
                 .<User, User>chunk(100)
                 .reader(userReader())
                 .processor(userProcessor())
                 .writer(userWriter())
                 .build();
+    }
+
+    private ItemReader<? extends User> userReader() throws Exception {
+        JpaPagingItemReader<User> userReader =  new JpaPagingItemReaderBuilder<User>()
+                .queryString("select u from User u")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(100)
+                .name("userItemReader")
+                .build();
+
+        userReader.afterPropertiesSet();
+        return userReader;
     }
 
 }
