b8c860e3de06498a7bcb9a556ef428f012723f6d
김주언 <wndjs803@naver.com>
2023-10-18 00:49:42 +0900

    fix: AuthController hateoas 제거

--- src/main/java/com/techeer/port/voilio/domain/user/controller/AuthController.java
+++ src/main/java/com/techeer/port/voilio/domain/user/controller/AuthController.java
@@ -1,15 +1,11 @@
 package com.techeer.port.voilio.domain.user.controller;
 
-import static com.techeer.port.voilio.global.result.ResultCode.LOGIN_SUCCESS;
-import static com.techeer.port.voilio.global.result.ResultCode.USER_REGISTRATION_SUCCESS;
-
 import com.techeer.port.voilio.domain.user.dto.request.UserLoginRequest;
 import com.techeer.port.voilio.domain.user.dto.request.UserSignUpRequest;
 import com.techeer.port.voilio.domain.user.dto.response.UserResponse;
 import com.techeer.port.voilio.domain.user.service.AuthService;
 import com.techeer.port.voilio.global.config.security.TokenDto;
 import com.techeer.port.voilio.global.result.ResultResponse;
-import java.rmi.AlreadyBoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.rmi.AlreadyBoundException;
+
+import static com.techeer.port.voilio.global.result.ResultCode.LOGIN_SUCCESS;
+import static com.techeer.port.voilio.global.result.ResultCode.USER_REGISTRATION_SUCCESS;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -42,8 +43,6 @@ public class AuthController {
       @RequestBody UserLoginRequest userLoginRequest) throws AlreadyBoundException {
     TokenDto tokenDto = authService.login(userLoginRequest);
     ResultResponse<TokenDto> resultResponse = new ResultResponse<>(LOGIN_SUCCESS, tokenDto);
-    //    resultResponse.add(
-    //        linkTo(methodOn(AuthController.class).login(userLoginRequest)).withSelfRel());
 
     return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
   }
