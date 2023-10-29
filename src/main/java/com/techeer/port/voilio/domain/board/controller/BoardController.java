5e19c5f5b23ff9fc79ed54e8a192f5359fe9c823
김주언 <wndjs803@naver.com>
2023-10-29 21:08:52 +0900

    refactor: hateoas 제거

--- src/main/java/com/techeer/port/voilio/domain/board/controller/BoardController.java
+++ src/main/java/com/techeer/port/voilio/domain/board/controller/BoardController.java
@@ -1,17 +1,14 @@
 package com.techeer.port.voilio.domain.board.controller;
 
-import static com.techeer.port.voilio.domain.user.entity.QUser.user;
-import static com.techeer.port.voilio.global.result.ResultCode.*;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 import com.techeer.port.voilio.domain.board.dto.BoardDto;
 import com.techeer.port.voilio.domain.board.dto.BoardThumbnailDto;
 import com.techeer.port.voilio.domain.board.dto.BoardVideoDto;
 import com.techeer.port.voilio.domain.board.dto.request.BoardCreateRequest;
+import com.techeer.port.voilio.domain.board.entity.Board;
 import com.techeer.port.voilio.domain.board.service.BoardService;
 import com.techeer.port.voilio.domain.user.entity.User;
 import com.techeer.port.voilio.domain.user.service.UserService;
+import com.techeer.port.voilio.global.common.Category;
 import com.techeer.port.voilio.global.config.security.JwtProvider;
 import com.techeer.port.voilio.global.error.ErrorCode;
 import com.techeer.port.voilio.global.error.exception.BusinessException;
@@ -20,11 +17,11 @@ import com.techeer.port.voilio.global.result.ResultsResponse;
 import com.techeer.port.voilio.s3.util.S3Manager;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -33,6 +30,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+
+import static com.techeer.port.voilio.global.result.ResultCode.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Tag(name = "Board", description = "Board API Document")
 @RequiredArgsConstructor
@@ -221,48 +224,17 @@ public class BoardController {
     return ResponseEntity.ok(ResultsResponse.of(BOARD_FIND_SUCCESS, allBoard));
   }
 
-  //    @GetMapping("/lists/category")
-  //    @Operation(summary = "카테고리 별 게시물 출력", description = "카테고리 별 게시물 출력 메서드입니다.")
-  //    public ResponseEntity<ResultResponse<Pagination<EntityModel<BoardResponse>>>>
-  // findBoardByCategory(
-  //            @RequestParam("category") String category,
-  //            @RequestParam(defaultValue = "0") int page,
-  //            @RequestParam(defaultValue = "30") int size,
-  //            @RequestHeader(value = "Authorization", required = false, defaultValue = "")
-  //            String authorizationHeader) {
-  //
-  //        Category category1 = Category.valueOf(category.toUpperCase());
-  //        Page<Board> boardPage = boardService.findBoardByCategory(category1, PageRequest.of(page,
-  // size));
-  //        List<EntityModel<BoardResponse>> boardLists =
-  //                boardPage.getContent().stream()
-  //                        .map(
-  //                                board ->
-  //                                        EntityModel.of(
-  //                                                boardMapper.toDto(board),
-  //                                                linkTo(
-  //                                                        methodOn(BoardController.class)
-  //                                                                .findBoardById(board.getId(),
-  // authorizationHeader))
-  //                                                        .withSelfRel()))
-  //                        .collect(Collectors.toList());
-  //
-  //        Pagination<EntityModel<BoardResponse>> result =
-  //                new Pagination<>(
-  //                        boardLists,
-  //                        boardPage.getNumber(),
-  //                        boardPage.getSize(),
-  //                        boardPage.getTotalElements(),
-  //                        boardPage.getTotalPages(),
-  //                        linkTo(
-  //                                methodOn(BoardController.class)
-  //                                        .findBoardByCategory(category, page, size,
-  // authorizationHeader))
-  //                                .withSelfRel());
-  //
-  //        ResultResponse<Pagination<EntityModel<BoardResponse>>> resultResponse =
-  //                new ResultResponse<>(BOARD_FIND_SUCCESS, result);
-  //
-  //        return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
-  //    }
+  @GetMapping("/lists/category")
+  @Operation(summary = "카테고리 별 게시물 출력", description = "카테고리 별 게시물 출력 메서드입니다.")
+  public ResponseEntity<ResultsResponse> findBoardByCategory(
+          @RequestParam("category") String category,
+          @RequestParam(defaultValue = "0") int page,
+          @RequestParam(defaultValue = "30") int size,
+          @RequestHeader(value = "Authorization", required = false, defaultValue = "")
+          String authorizationHeader) {
+
+    Category category1 = Category.valueOf(category.toUpperCase());
+    Page<Board> boardPage = boardService.findBoardByCategory(category1, PageRequest.of(page, size));
+    return ResponseEntity.ok(ResultsResponse.of(BOARD_FIND_SUCCESS, boardPage));
+  }
 }
