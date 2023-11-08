beb23eae47344ec8a4a9bde15cbf37fae5fe1c2b
kimich <ickim1218@gmail.com>
2023-11-09 00:11:26 +0900

    fix: boardImage entity에 생성자 추가

--- src/main/java/com/techeer/port/voilio/domain/board/entity/BoardImage.java
+++ src/main/java/com/techeer/port/voilio/domain/board/entity/BoardImage.java
@@ -2,16 +2,21 @@ package com.techeer.port.voilio.domain.board.entity;
 
 import com.techeer.port.voilio.global.common.BaseEntity;
 import javax.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "board_images")
 public class BoardImage extends BaseEntity {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "board_image_id")
   private Long id;
 
@@ -20,4 +25,9 @@ public class BoardImage extends BaseEntity {
   private Board board;
 
   private String url;
+
+  public BoardImage(Board board, String url) {
+    this.board = board;
+    this.url = url;
+  }
 }
