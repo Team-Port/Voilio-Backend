5431e58b95ee8f11811982eeabce35c6a6d19eac
kimich1218 <ickim1218@gmail.com>
2023-11-13 16:09:19 +0900

    refactor: controller, repository, updateRequest 추가

--- src/main/java/com/techeer/port/voilio/domain/board/repository/BoardImageRepository.java
+++ src/main/java/com/techeer/port/voilio/domain/board/repository/BoardImageRepository.java
@@ -1,8 +1,15 @@
 package com.techeer.port.voilio.domain.board.repository;
 
+import com.techeer.port.voilio.domain.board.entity.Board;
 import com.techeer.port.voilio.domain.board.entity.BoardImage;
+import java.util.List;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardImageRepository extends JpaRepository<BoardImage, Long> {}
+public interface BoardImageRepository extends JpaRepository<BoardImage, Long> {
+
+  List<BoardImage> findByBoard(Board board);
+
+}
