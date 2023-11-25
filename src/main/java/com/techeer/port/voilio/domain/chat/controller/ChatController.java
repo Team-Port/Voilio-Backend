f2983a575aea1425b0884a5fb5bd591d9f2fae55
김주언 <wndjs803@naver.com>
2023-11-25 19:17:35 +0900

    feat: rabbitmq 시도중

--- src/main/java/com/techeer/port/voilio/domain/chat/controller/ChatController.java
+++ src/main/java/com/techeer/port/voilio/domain/chat/controller/ChatController.java
@@ -4,8 +4,10 @@ import com.techeer.port.voilio.domain.chat.entity.ChatMessage;
 import com.techeer.port.voilio.domain.chat.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
@@ -16,15 +18,17 @@ public class ChatController {
 
   private final SimpMessageSendingOperations template;
   private final ChatMessageService chatMessageService;
+  private final RabbitTemplate rabbitTemplate;
 
   // pub/chat/1/message => 1번 방으로 publish한 메세지 처리
   @MessageMapping("/chat/{chatRoomId}/message")
-  public void message(ChatMessage message, @DestinationVariable Long chatRoomId) {
+  public void message(@Payload ChatMessage message, @DestinationVariable Long chatRoomId) {
     System.out.println(message.getMessage());
-    chatMessageService.saveChatMessage(message);
+//    chatMessageService.saveChatMessage(message);
 
     // sub/1을 subscibe한 유저에게 메세지 전송
     template.convertAndSend("/sub/" + chatRoomId.toString(), message);
+    rabbitTemplate.convertAndSend("chat.messages", message);
   }
 
   @MessageMapping("chat/{chatRoomId}/enter")
--- src/main/java/com/techeer/port/voilio/domain/chat/service/ChatMessageService.java
+++ src/main/java/com/techeer/port/voilio/domain/chat/service/ChatMessageService.java
@@ -3,6 +3,8 @@ package com.techeer.port.voilio.domain.chat.service;
 import com.techeer.port.voilio.domain.chat.entity.ChatMessage;
 import com.techeer.port.voilio.domain.chat.repository.ChatMessageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +13,8 @@ public class ChatMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
 
+    @Async
+    @RabbitListener(queues = "chat.messages")
     public void saveChatMessage(ChatMessage message){
         chatMessageRepository.save(message);
     }
